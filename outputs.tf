59064178469905e2de73bbc02547029225d6a471
Daniel Allen <dan@aeternagroup.net>
2026-08-15 00:53:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "site_recovery_replication_recovery_plans_id" {
 }
 output "site_recovery_replication_recovery_plans_azure_to_azure_settings" {
   description = "Map of azure_to_azure_settings values across all site_recovery_replication_recovery_plans, keyed the same as var.site_recovery_replication_recovery_plans"
-  value       = { for k, v in azurerm_site_recovery_replication_recovery_plan.site_recovery_replication_recovery_plans : k => v.azure_to_azure_settings if v.azure_to_azure_settings != null && length(v.azure_to_azure_settings) > 0 }
+  value       = { for k, v in azurerm_site_recovery_replication_recovery_plan.site_recovery_replication_recovery_plans : k => one(v.azure_to_azure_settings) if v.azure_to_azure_settings != null && length(v.azure_to_azure_settings) > 0 }
 }
 output "site_recovery_replication_recovery_plans_boot_recovery_group" {
   description = "Map of boot_recovery_group values across all site_recovery_replication_recovery_plans, keyed the same as var.site_recovery_replication_recovery_plans"
@@ -12,7 +12,7 @@ output "site_recovery_replication_recovery_plans_boot_recovery_group" {
 }
 output "site_recovery_replication_recovery_plans_failover_recovery_group" {
   description = "Map of failover_recovery_group values across all site_recovery_replication_recovery_plans, keyed the same as var.site_recovery_replication_recovery_plans"
-  value       = { for k, v in azurerm_site_recovery_replication_recovery_plan.site_recovery_replication_recovery_plans : k => v.failover_recovery_group if v.failover_recovery_group != null && length(v.failover_recovery_group) > 0 }
+  value       = { for k, v in azurerm_site_recovery_replication_recovery_plan.site_recovery_replication_recovery_plans : k => one(v.failover_recovery_group) if v.failover_recovery_group != null && length(v.failover_recovery_group) > 0 }
 }
 output "site_recovery_replication_recovery_plans_name" {
   description = "Map of name values across all site_recovery_replication_recovery_plans, keyed the same as var.site_recovery_replication_recovery_plans"
@@ -24,7 +24,7 @@ output "site_recovery_replication_recovery_plans_recovery_vault_id" {
 }
 output "site_recovery_replication_recovery_plans_shutdown_recovery_group" {
   description = "Map of shutdown_recovery_group values across all site_recovery_replication_recovery_plans, keyed the same as var.site_recovery_replication_recovery_plans"
-  value       = { for k, v in azurerm_site_recovery_replication_recovery_plan.site_recovery_replication_recovery_plans : k => v.shutdown_recovery_group if v.shutdown_recovery_group != null && length(v.shutdown_recovery_group) > 0 }
+  value       = { for k, v in azurerm_site_recovery_replication_recovery_plan.site_recovery_replication_recovery_plans : k => one(v.shutdown_recovery_group) if v.shutdown_recovery_group != null && length(v.shutdown_recovery_group) > 0 }
 }
 output "site_recovery_replication_recovery_plans_source_recovery_fabric_id" {
   description = "Map of source_recovery_fabric_id values across all site_recovery_replication_recovery_plans, keyed the same as var.site_recovery_replication_recovery_plans"
